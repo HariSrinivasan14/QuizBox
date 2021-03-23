ef0cc69c92dbbfdb24e3c27982d3ff93420594dd
Hari Srinivasan <hari.srinivasan@mail.utoronto.ca>
2021-03-23 18:43:25 -0400

    small changes to sample question

--- pub/js/examples.js
+++ pub/js/examples.js
@@ -17,7 +17,7 @@ newQuiz.createMultipleChoiceOne("What does CSS stand for?", ["Computer Style She
 newQuiz.createMultipleChoiceMany("Which of the following cities are located in Ontario? Select all that apply.", ["Toronto", "Brampton", "Hamilton", "Mississauga", "New York"], ["Toronto", "Brampton", "Hamilton", "Mississauga"]);
 
 
-newQuiz.createTrueOrFalseQuestion(["Yellow is a primary color.", "CPU stands for Central Processing Unit.", "An Octopus has three hearts.", "The letter A is the most common letter used in the English language."], [true, true, true, false]);
+newQuiz.createTrueOrFalseQuestion(["Yellow is a primary colour.", "CPU stands for Central Processing Unit.", "An Octopus has three hearts.", "The letter A is the most common letter used in the English language."], [true, true, true, false]);
 
 
 newQuiz.createSequenceQuestion(["Invention of the Turing Machine.", "American Civil War.", "Extinction of Dinosaurs.", "First time landing on the Moon", "COVID-19."], ["COVID-19.", "First time landing on the Moon", "Invention of the Turing Machine.", "American Civil War.", "Extinction of Dinosaurs."]);
