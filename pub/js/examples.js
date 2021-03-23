d1c15ce65d5534063af184d3c4bc1ba8abe15f4d
Hari Srinivasan <hari.srinivasan@mail.utoronto.ca>
2021-03-23 14:09:17 -0400

    changed sample question and fixed some bugs

--- pub/js/examples.js
+++ pub/js/examples.js
@@ -9,13 +9,15 @@ document.querySelector('body').appendChild(tempDiv);
 
 const newQuiz = new QuizBox(tempDiv);
 
-newQuiz.createMultipleChoiceOne("This is a test question. Please choose one of the following options", ["option 1", "option 2", "option 3", "option 4", "option 0"], "option 2");
+newQuiz.createMultipleChoiceOne("What does HTML stand for?", ["HyperText Machine Learning", "HyperText Markup Language", "Hyper Machine Language", "None of the above", "All of the above"], "HyperText Markup Language");
+
+newQuiz.createMultipleChoiceOne("What does CSS stand for?", ["Computer Style Sheets", "Colour Style Sheets", "Cascading Style Sheets", "None of the above"], "Cascading Style Sheets");
 
 
 newQuiz.createMultipleChoiceMany("Which of the following cities are located in Ontario? Select all that apply.", ["Toronto", "Brampton", "Hamilton", "Mississauga", "New York"], ["Toronto", "Brampton", "Hamilton", "Mississauga"]);
 
-// question take from https://www.cosmopolitan.com/uk/worklife/a32612392/best-true-false-quiz-questions/
-newQuiz.createTrueOrFalseQuestion(["Australia is wider than the moon.", "An octopus has three hearts.", "Bananas are curved because they grow upwards towards the sun.", "the letter A is the most common letter used in the English language."], [true, true, true, false]);
+
+newQuiz.createTrueOrFalseQuestion(["Yellow is a primary color", "CPU stands for Central Processing Unit", "An Octopus has three hearts", "the letter A is the most common letter used in the English language."], [true, true, true, false]);
 
 
 newQuiz.createSequenceQuestion(["Invention of the Turing Machine.", "American Civil War.", "Extinction of Dinosaurs.", "First time landing on the Moon", "COVID-19."], ["COVID-19.", "First time landing on the Moon", "Invention of the Turing Machine.", "American Civil War.", "Extinction of Dinosaurs."]);
