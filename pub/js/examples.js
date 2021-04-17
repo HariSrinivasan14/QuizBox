bb8f01ea0e2ba6643ed39d48bf92749e117291d3
Hari Srinivasan <hari.srinivasan@mail.utoronto.ca>
2021-04-17 00:18:27 -0400

    added example code to the example section

--- pub/js/examples.js
+++ pub/js/examples.js
@@ -31,30 +31,39 @@ newQuiz2.createMultipleChoiceOne("What does HTML stand for?", ["HyperText Machin
 
 newQuiz2.createMultipleChoiceOne("What does CSS stand for?", ["Computer Style Sheets", "Colour Style Sheets", "Cascading Style Sheets", "None of the above"], "Cascading Style Sheets");
 
+
+
+
 const tempDiv3 = document.querySelector('#examplesDiv3');
 const newQuiz3 = new QuizBox(tempDiv3);
 newQuiz3.createMultipleChoiceOne("What does HTML stand for?", ["HyperText Machine Learning", "HyperText Markup Language", "Hyper Machine Language", "None of the above", "All of the above"], "HyperText Markup Language");
+newQuiz3.createMultipleChoiceOne("What does CSS stand for?", ["Computer Style Sheets", "Colour Style Sheets", "Cascading Style Sheets", "None of the above"], "Cascading Style Sheets");
+
 
 
 const tempDiv4 = document.querySelector('#examplesDiv4');
 const newQuiz4 = new QuizBox(tempDiv4);
 newQuiz4.createMultipleChoiceOne("What does HTML stand for?", ["HyperText Machine Learning", "HyperText Markup Language", "Hyper Machine Language", "None of the above", "All of the above"], "HyperText Markup Language");
+newQuiz4.createMultipleChoiceOne("What does CSS stand for?", ["Computer Style Sheets", "Colour Style Sheets", "Cascading Style Sheets", "None of the above"], "Cascading Style Sheets");
 newQuiz4.setAnimationAll("Scroll")
 
 const tempDiv5 = document.querySelector('#examplesDiv5');
 const newQuiz5 = new QuizBox(tempDiv5);
 newQuiz5.createMultipleChoiceOne("What does HTML stand for?", ["HyperText Machine Learning", "HyperText Markup Language", "Hyper Machine Language", "None of the above", "All of the above"], "HyperText Markup Language");
+newQuiz5.createMultipleChoiceOne("What does CSS stand for?", ["Computer Style Sheets", "Colour Style Sheets", "Cascading Style Sheets", "None of the above"], "Cascading Style Sheets");
 newQuiz5.setAnimationAll("Reveal")
 
 
 const tempDiv6 = document.querySelector('#examplesDiv6');
 const newQuiz6 = new QuizBox(tempDiv6);
 newQuiz6.createMultipleChoiceOne("What does HTML stand for?", ["HyperText Machine Learning", "HyperText Markup Language", "Hyper Machine Language", "None of the above", "All of the above"], "HyperText Markup Language");
+newQuiz6.createMultipleChoiceOne("What does CSS stand for?", ["Computer Style Sheets", "Colour Style Sheets", "Cascading Style Sheets", "None of the above"], "Cascading Style Sheets");
 newQuiz6.setAnimationAll("Slide Down")
 
 const tempDiv7 = document.querySelector('#examplesDiv7');
 const newQuiz7 = new QuizBox(tempDiv7);
 newQuiz7.createMultipleChoiceOne("What does HTML stand for?", ["HyperText Machine Learning", "HyperText Markup Language", "Hyper Machine Language", "None of the above", "All of the above"], "HyperText Markup Language");
+newQuiz7.createMultipleChoiceOne("What does CSS stand for?", ["Computer Style Sheets", "Colour Style Sheets", "Cascading Style Sheets", "None of the above"], "Cascading Style Sheets");
 newQuiz7.setAnimationAll("Fade In")
 
 
